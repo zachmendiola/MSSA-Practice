4ffebf3af0141f5b2b57c549bdc2c7b704aadee9
Zachary Mendiola <60198954+zachmendiola@users.noreply.github.com>
2020-02-04 10:33:40 -0800

    Add files via upload
    Multiple loop counter that counts 0-10 and then 10-0, five times.

---
+++ Multiple Counter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Multiple_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Encasing for loop that contains the larger iterations and increments each time the counter goes up and down.
+            for (int j = 0; j < 5; j++)
+            {
+                //For loop that counts up to 10.
+                for (int i = 0; i <= 10; i++)
+                {
+                    //Prints value of 'i' to console.
+                    Console.WriteLine(i);
+                }
+                //For loop that counts down to 0.
+                for (int i = 10; i >= 0; i--)
+                {
+                    //Prints value of 'i' to console.
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
