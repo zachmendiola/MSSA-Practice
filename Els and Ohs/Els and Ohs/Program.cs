2a282cd0f93ea0f50bf5f712cbe4c26ee315712d
Zach Mendiola <zach.mendiola27@outlook.com>
2020-02-28 09:24:32 -0800

    Initial upload of the file

---
+++ Els and Ohs/Els and Ohs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Els_and_Ohs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            foreach (var item in el(arr1))
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static int[] el(int[] arr)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                arr[arr.Length - 1] = 1;
+                if (i == 0)
+                {
+                    arr[i] = 1;
+                    count++;
+                }
+                else if (arr[i-1] == 1)
+                {
+                    continue;
+                }
+                else if (arr[i-1] == 0 && arr[i+1] == 0)
+                {
+                    arr[i] = 1;
+                    count++;
+                }
+            }
+            Console.WriteLine(arr.Length);
+            Console.WriteLine(count);
+            return arr;
+        }
+    }
+}
