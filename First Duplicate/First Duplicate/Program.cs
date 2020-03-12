ea1b061eda86809321b1c12c65ce148cd473aa23
Zach Mendiola <zach.mendiola27@outlook.com>
2020-03-12 15:29:50 -0700

    Initial Upload of the files
    Solution to the "First Duplicate" problem from leetcode. Objective is to find the first character that is not a duplicate in a string.

---
+++ First Duplicate/First Duplicate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace First_Duplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(FirstDup("aabbccjkklo"));
+        }
+        static char FirstDup(string str)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!dict.ContainsKey(str[i]))
+                {
+                    dict.Add(str[i], 1);
+                }
+                else
+                {
+                    dict[str[i]]++;
+                }
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (dict.ContainsKey(str[i]) && dict[str[i]] == 1)
+                {
+                    return str[i];
+                }
+            }
+            return '.';
+        }
+    }
+}
