2083a1dec0c930b3324d1e6ac7471e2956a0d35d
Zach Mendiola <zach.mendiola27@outlook.com>
2020-03-18 15:58:26 -0700

    Initial upload

---
+++ Technical Mentor Call 18 Mar/Technical Mentor Call 18 Mar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Technical_Mentor_Call_18_Mar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(IsUnique("dsfhlkj"));
+            Console.WriteLine(IsUnique("llll"));
+        }
+        // Method String If Unique 
+        static bool IsUnique(string str)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (dict.ContainsKey(str[i]))
+                {
+                    return false;
+                }
+                else
+                {
+                    dict.Add(str[i], 1);
+                }
+            }
+            return true;
+        }
+
+    }
+}
