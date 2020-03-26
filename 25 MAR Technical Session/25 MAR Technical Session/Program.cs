a41382a8878bda8ca40908fd67c312b54b9eadb2
Zach Mendiola <zach.mendiola27@outlook.com>
2020-03-26 08:28:58 -0700

    Initial upload

---
+++ 25 MAR Technical Session/25 MAR Technical Session/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _25_MAR_Technical_Session
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //accept two integers and return remainder
+            Console.WriteLine("Hello World!");
+            Remaining(1, 1);
+            Remaining(1, 0);
+            Console.WriteLine(DuplicateRemover("hheelloo"));
+        }
+        static int Remaining(int a, int b)
+        {
+            try
+            {
+                int result = a % b;
+                return result;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error: Please enter a number other than zero for the divisor");
+            }
+            return 0;
+        }
+        static string DuplicateRemover(string a)
+        {
+            List<char> list = new List<char>();
+            string newString = string.Empty;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (!list.Contains(a[i]))
+                {
+                    list.Add(a[i]);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            foreach (char item in list)
+            {
+                newString += item;
+            }
+            return newString;
+        }
+    }
+}
