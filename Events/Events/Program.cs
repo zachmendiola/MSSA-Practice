c4ec9feeda89815d9740c9e00f9d7ef24ec31b5d
Zach Mendiola <zach.mendiola27@outlook.com>
2020-03-29 21:12:41 -0700

    Initial Upload

---
+++ Events/Events/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+            int c = guess(ref a, b);
+            Console.WriteLine("{0}, {1}, {2}", a, b, c);
+        }
+        public static int guess(ref int first, int second)
+
+        {
+            int temp = first;
+            first = second;
+            second = temp;
+            return temp;
+        }
+
+
+    }
+    class Burger
+    {
+        public string cheese { get; set; }
+        public string lettuce { get; set; }
+        public string meat { get; set; }
+    }
+   
+
+}
