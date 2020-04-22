bfa200d9efe8d7fb35937ed503d1447d07189c03
Zach Mendiola <zach.mendiola27@outlook.com>
2020-04-22 13:11:53 -0700

    Initial upload

---
+++ 22 Tech Interview/22 Tech Interview/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+
+namespace _22_Tech_Interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Stack<int> myStack = new Stack<int>(10);
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            Console.WriteLine(myStack.Pop());
+            Console.WriteLine(myStack.Pop());
+            Console.WriteLine(myStack.Pop());
+
+        }
+    }
+    //LIFO
+    //Push
+    //Pop
+    //Peek
+    //Generic
+    class Stack<T>
+    {
+        T[] arr;
+        int pointer = 0;
+        public Stack(int size)
+        {
+           this.arr = new T[size];
+        }
+        public void Push(T data)
+        {
+            arr[pointer++] = data;
+        }
+        public T Pop()
+        {
+            return arr[--pointer];
+        }
+        public T Peek()
+        {
+            return arr[pointer-1];
+        }
+    }
+}
